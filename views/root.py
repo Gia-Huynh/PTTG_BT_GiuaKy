3271b587126846a00f4120f82bf0e201048d5f0e
Nguyen Duy Quang <43096050+YumeAyasaki@users.noreply.github.com>
2023-11-14 23:07:12 +0700

    Update control replace
    With the risk of memory leak, the control now works as intended.

--- views/root.py
+++ views/root.py
@@ -15,26 +15,36 @@ class Root(Tk):
 
         self.screen_status = Screen.DEFAULT
 
-        self.control = Control_View(self)
-        self.control.place(relwidth=0.2, relheight=1)
+        # Store the control
+        self.controls = {
+            Screen.DEFAULT: Control_View(self),
+            Screen.SVD: SVD_Control_View(self),
+            Screen.PCA: None, # TODO
+        }
+
+        for control in self.controls.values():
+            if control is not None:
+                control.place(relwidth=0.2, relheight=1)
+
+        self.update_control()
 
         self.display = Image_View(self)
         self.display.place(relwidth=0.8, relheight=1, relx=0.2)
 
     def set_svd(self):
         self.screen_status = Screen.SVD
-        self.control.destroy()
-        self.control = SVD_Control_View(self)
-        self.control.place(relwidth=0.2, relheight=1)
+        self.update_control()
 
     def set_pca(self):
         self.screen_status = Screen.PCA
 
     def set_default(self):
         self.screen_status = Screen.DEFAULT
-        self.control.destroy()
-        self.control = Control_View(self)
-        self.control.place(relwidth=0.2, relheight=1)
+        self.update_control()
+
+    def update_control(self):
+        self.control = self.controls[self.screen_status]
+        self.control.tkraise()
 
     def start(self):
         self.mainloop()

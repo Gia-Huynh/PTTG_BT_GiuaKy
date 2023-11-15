7e50561c7e67d820419eaf6dc8126e5fdc7709e1
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2023-11-15 22:41:00 +0700

    Now return name instead of class num

--- EigenFace.py
+++ EigenFace.py
@@ -11,12 +11,15 @@ from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
-Num_Eigenface = 100
+Num_Eigenface = 50
+#100 in production
+min_faces = 100
+#70 in production
 class EigenFace:
     def __init__(self):
         #self.name =  name
         #lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-        lfw_people = fetch_lfw_people(min_faces_per_person=70)
+        lfw_people = fetch_lfw_people(min_faces_per_person=min_faces)
         n_samples, h, w = lfw_people.images.shape
         X = lfw_people.data
         n_features = X.shape[1]
@@ -24,33 +27,26 @@ class EigenFace:
         target_names = lfw_people.target_names
         n_classes = target_names.shape[0]
 
-        print("Total dataset size:")
-        print ("Dataset Shape:",X.shape,", h:",h,", w:",w)
-        print("n_samples: %d" % n_samples)
-        print("n_features: %d" % n_features)
-        print("n_classes: %d" % n_classes)
+        print("Chi tiết Dataset:")
+        print("Số lượng mẫu: %d" % n_samples)
+        print("Số đặc trưng: %d" % n_features)
+        print("Số lớp: %d" % n_classes)
+        print ("Kích thước dataset:",X.shape,", h:",h,", w:",w)
 
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.20, random_state=69)
-        scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
+        self.scaler = StandardScaler()
+        X_train = self.scaler.fit_transform(X_train)
+        X_test = self.scaler.transform(X_test)
         n_components = Num_Eigenface
         #150
-        print(
-            "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
-        )
-        t0 = time()
+        print("Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0]))
         pca = PCA(n_components=n_components, svd_solver="randomized", whiten=True).fit(X_train)
-        print("done in %0.3fs" % (time() - t0))
         eigenfaces = pca.components_.reshape((n_components, h, w))
         print("Projecting the input data on the eigenfaces orthonormal basis")
-        t0 = time()
         X_train_pca = pca.transform(X_train)
         X_test_pca = pca.transform(X_test)
-        print("done in %0.3fs" % (time() - t0))
         print("Fitting the classifier to the training set")
-        t0 = time()
         param_grid = {
             "C": loguniform(1e3, 1e5),
             "gamma": loguniform(1e-4, 1e-1),
@@ -59,19 +55,12 @@ class EigenFace:
             SVC(kernel="rbf", class_weight="balanced"), param_grid, n_iter=10
         )
         clf = clf.fit(X_train_pca, y_train)
-        print("done in %0.3fs" % (time() - t0))
         print("Best estimator found by grid search:")
         print(clf.best_estimator_)
         print("Predicting people's names on the test set")
-        t0 = time()
         y_pred = clf.predict(X_test_pca)
-        print("done in %0.3fs" % (time() - t0))
         print(classification_report(y_test, y_pred, target_names=target_names))
-        #ConfusionMatrixDisplay.from_estimator(
-        #    clf, X_test_pca, y_test, display_labels=target_names, xticks_rotation="vertical"
-        #)
-        #plt.tight_layout()
-        #plt.show()
+        
         self.clf = clf
         self.pca = pca
         self.target_names = target_names
@@ -83,15 +72,18 @@ class EigenFace:
     def predict_img_path (self, imgPath):
         #inpImg: Greyscale, size: 62 x 47
         #"lfw\\Aaron_Eckhart\\Aaron_Eckhart_0001.jpg"
-        #.reshape(-1, 1)
         img2D = np.mean(cv2.resize (cv2.imread (imgPath), (47, 62)), axis = 2)
         img1D = np.ravel (img2D).reshape(1, -1)
+        img1D = self.scaler.transform(img1D)
         img1d_pca = self.pca.transform(img1D)
         result = self.clf.predict(img1d_pca)
-        return result
+        return self.target_names[result[0]].rsplit(" ", 1)[-1]
     def predict_img_list (self, inpImg):
         pass
+    
 ey = EigenFace ()
+result = ey.predict_img_path("lfw\\Aaron_Eckhart\\Aaron_Eckhart_0001.jpg")
+
 """def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
     #Helper function to plot a gallery of portraits
     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))

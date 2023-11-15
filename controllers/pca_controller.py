f4e321fdbd96faad599bf47e3c721448a6ab72ca
Nguyen Duy Quang <43096050+YumeAyasaki@users.noreply.github.com>
2023-11-16 00:53:27 +0700

    PCA Visualize
    Add visualization for eigenface in PCA.

--- controllers/pca_controller.py
+++ controllers/pca_controller.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import numpy as np
+import cv2
 
 class PCA_Controller:
     def __init__(self, image_model, image_view, eigen_face):
@@ -21,11 +22,33 @@ class PCA_Controller:
         predict = self.pca.predict_img_path(self.image_model.get_image_path())
         self.image_view.control.update_predict(predict)
 
-        eigenface = self.pca.GetFeatureVectors(self.image_model.get_image_path())
-        eigenface = eigenface.reshape((62, 47))
-        print(eigenface)
-        # image = Image.fromarray(eigenface)
-        # self.image_view.display.set_image(image)
+        eigenface = self.pca.get_Eigenfaces_And_Feature(self.image_model.get_image_path())
+        n = len(eigenface)
+        eigenface_1 = eigenface[0][1]
+        eigenface_2 = eigenface[5][1]
+        for i in range(1, 5):
+            eigenface_1 = np.hstack((eigenface_1, eigenface[i][1]))
+        for i in range(6, n):
+            eigenface_2 = np.hstack((eigenface_2, eigenface[i][1]))
+        new_eigenface = np.vstack((eigenface_1, eigenface_2))
+        new_eigenface = self.normalized(new_eigenface)
+        original_image = self.image_model.get_image_cv()
+        new_height = int(original_image.shape[0] * new_eigenface.shape[1] / original_image.shape[1])
+        new_eigenface = cv2.resize(new_eigenface, (original_image.shape[1], new_height))
+        new_eigenface = np.vstack((original_image, new_eigenface))
+
+        image = Image.fromarray(new_eigenface)
+        self.image_view.display.set_image(image)
+
+        values = [eigenface[i][0] for i in range(n)]
+        print(values)
+
+    def normalized(self, img):
+        img = img.astype(np.float32)
+        img = (img - np.min(img)) / (np.max(img) - np.min(img))
+        img = img * 255
+        img = img.astype(np.uint8)
+        return img
 
     def back(self, event):
         self.image_view.set_default()
